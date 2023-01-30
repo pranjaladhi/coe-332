009ae322e8c82524d34f356e00a8182a159c42e2
Pranjal Adhikari <pranjal.adhikari@gmail.com>
2023-01-30 16:53:48 -0600

    generating random sites

---
+++ homework02/site_generation.py
@@ -0,0 +1,11 @@
+#Pranjal Adhikari
+
+import random
+import json
+
+meteorite_types = ["stony", "iron", "stony-iron"]
+
+sites = 
+{
+    "sites": [ {"site_id": 1, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition:" random ]
+}
