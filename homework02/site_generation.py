ad64422a0d9a660c173dc39d0f5fa2ae86349717
Pranjal Adhikari <pranjal.adhikari@gmail.com>
2023-01-30 17:16:21 -0600

    added generating random sites

--- homework02/site_generation.py
+++ homework02/site_generation.py
@@ -3,9 +3,17 @@
 import random
 import json
 
-meteorite_types = ["stony", "iron", "stony-iron"]
+meteorite_compositions = ["stony", "iron", "stony-iron"]
 
-sites = 
-{
-    "sites": [ {"site_id": 1, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition:" random ]
+random_sites = { "sites": 
+    [ 
+        {"site_id": 1, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
+        {"site_id": 2, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
+        {"site_id": 3, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
+        {"site_id": 4, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
+        {"site_id": 5, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
+    ]
 }
+
+with open('random_sites.json', 'w') as out:
+    json.dump(random_sites, out, indent = 2)
