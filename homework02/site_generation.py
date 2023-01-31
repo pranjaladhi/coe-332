704bb71b6a5c04a77a0abd547f47694c15a9f533
Pranjal Adhikari <pranjal.adhikari@gmail.com>
2023-01-30 22:50:56 -0600

    final site_gen and trip_time

--- homework02/site_generation.py
+++ homework02/site_generation.py
@@ -3,9 +3,9 @@
 import random
 import json
 
-meteorite_compositions = ["stony", "iron", "stony-iron"]
+meteorite_compositions = ["stony", "iron", "stony-iron"] #creating a list of the meteorite types that are present 
 
-random_sites = { "sites": 
+random_sites = { "sites": #creating dictionary of 5 total sites, which the coordinates of and mateorite composition (from the meteorite list) found in each site is generated randomly 
     [ 
         {"site_id": 1, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
         {"site_id": 2, "latitude": random.uniform(16.0, 18.0), "longitude": random.uniform(82.0, 84.0), "meteorite composition": random.choice(meteorite_compositions) },
@@ -15,5 +15,5 @@ random_sites = { "sites":
     ]
 }
 
-with open('random_sites.json', 'w') as out:
+with open('random_sites.json', 'w') as out: #dumping the randomly generated site data into a json file
     json.dump(random_sites, out, indent = 2)
--- homework02/trip_time.py
+++ homework02/trip_time.py
@@ -3,32 +3,32 @@
 import json
 import math
 
-with open ('random_sites.json', 'r') as s:
+with open ('random_sites.json', 'r') as s: #utilizing the randomly generated data of the sites in 
     sites_d = json.load(s)
 
 print("\n-----ROBOT SITES VISIT SUMMARY-----\n")
 
-lat1 = 16.0
+lat1 = 16.0 #initial coordinates of the robot (starting location)
 lon1 = 82.0
-max_speed = 10
+max_speed = 10 #used to determine the time it takes for the robot to travel a given distance
 mars_radius = 3389.5
 
-def calc_gcd(latitude_1: float, longitude_1: float, latitude_2: float, longitude_2: float) -> float:
+def calc_gcd(latitude_1: float, longitude_1: float, latitude_2: float, longitude_2: float) -> float: #calculate the distance between coordinates (lat1, lon1) and (lat2, lon2) on Mars
     lat1, lon1, lat2, lon2 = map( math.radians, [latitude_1, longitude_1, latitude_2, longitude_2] )
     d_sigma = math.acos( math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2) * math.cos(abs(lon1-lon2)))
     return ( mars_radius * d_sigma )
 
 total_travel_time = 0
-for i in range (len(sites_d['sites'])): #stony-iron sample time
+for i in range (len(sites_d['sites'])): #iterating (or traveling) through the 5 generated sites from the starting point of (lat1, lon1)
     lat2 = sites_d['sites'][i]['latitude']
     lon2 = sites_d['sites'][i]['longitude']
     
-    time_to_site = calc_gcd(lat1, lon1, lat2, lon2) / max_speed
-    total_travel_time += time_to_site
+    time_to_site = calc_gcd(lat1, lon1, lat2, lon2) / max_speed #calculating the time taken by the robot to get to the site, from the prior site (or starting location)
+    total_travel_time += time_to_site #adding the travel time to the site, to the total time of the sites investigation
 
     print("Leg", i+1, ":   time to travel =", round(time_to_site, 2), "hr,", end = " ")
 
-    if sites_d['sites'][i]['meteorite composition'] == 'stony-iron':
+    if sites_d['sites'][i]['meteorite composition'] == 'stony-iron': #stony-iron sample time
         total_travel_time += 3
         print("time to sample =", "3 hr")
 
@@ -40,8 +40,8 @@ for i in range (len(sites_d['sites'])): #stony-iron sample time
         total_travel_time += 1
         print("time to sample =", "1 hr")
 
-    lat1 = lat2
+    lat1 = lat2 #setting the new starting location of the robot, after it has reached a site and collected the respective sample
     lon1 = lon2
 
 print("----------------------------------------------------------")
-print("Number of legs =", len(sites_d['sites']), ", total travel time =", round(total_travel_time, 2), "hr")
+print("Number of legs =", len(sites_d['sites']), ", total travel time =", round(total_travel_time, 2), "hr") #printing the total time it takes for the investigation of the 5 sites to occur
