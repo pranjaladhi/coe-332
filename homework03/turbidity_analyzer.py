7088e353ac2cd21d185cb5759a5c352886d58d52
Pranjal Adhikari <pranjal.adhikari@gmail.com>
2023-02-12 17:50:25 -0600

    added comments to analyzer

--- homework03/turbidity_analyzer.py
+++ homework03/turbidity_analyzer.py
@@ -34,21 +34,21 @@ def time_required(turb: float) -> float:
 
 def main():
     print("\n-----WATER TURBIDITY ANALYZER-----\n")
-    turbidity_data = requests.get(url='https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json')
-    i = len(turbidity_data.json()['turbidity_data']) - 5
+    turbidity_data = requests.get(url='https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json') #retrieving water data from the given url
+    i = len(turbidity_data.json()['turbidity_data']) - 5 #starting from the 5th most recent measurement
     water_turbidity = 0
-    while (i < (len(turbidity_data.json()['turbidity_data']))):
+    while (i < (len(turbidity_data.json()['turbidity_data']))): #iterating from the 5th to the most recent measurement
         a0 = turbidity_data.json()['turbidity_data'][i]['calibration_constant'] #calibration constant
         I90 = turbidity_data.json()['turbidity_data'][i]['detector_current'] #ninety degree detector current
         water_turbidity += current_water_turbidity(a0, I90)
         i += 1
-    water_turbidity = water_turbidity/5
+    water_turbidity = water_turbidity/5 #average of the 5 most recent measurements
     print("Current water turbidity (based on average of 5 recent measurements):", round(water_turbidity, 5), "NTU")
     ts = 1.0 #turbidity threshold
-    if ts < water_turbidity:
+    if ts < water_turbidity: #below the threshold
         print("*UNSAFE WARNING* Turbidity is above the threshold for safe use")
         print("Minimum time required to return below a safe threshold:", round(time_required(water_turbidity), 2), "hours")
-    else:
+    else: #above the threshold
         print("*SAFE* Turbidity is below the threshold for safe use")
         print("Minimum time required to return below a safe threshold: 0 hours")
 
