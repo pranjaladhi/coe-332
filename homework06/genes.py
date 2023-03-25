04684a1ae4a3a65f5b41a5062b2ee9da287c5f4d
Ubuntu <ubuntu@pa8729-vm.js2local>
2023-03-25 03:34:36 +0000

    delete route

--- homework06/genes.py
+++ homework06/genes.py
@@ -8,23 +8,38 @@ def get_redis_client():
 rd = get_redis_client()
 
 
+def get_method():
+    all_genes_data = rd.hgetall()
+    return f'test'
+
+def post_method():
+    global rd
+    response = requests.get(url='https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json')
+    all_genes_data = response.json().get('responseHeader')
+    rd.hset('genes', all_genes_data)
+    return f'test'
+
+def delete_method():
+    global rd
+    rd.flushdb()
+    return f'Data deleted'
+
 @app.route('/data', methods = ['GET', 'POST', 'DELETE'])
 def data_requests():
     global rd
     if request.method == 'GET':
-        all_gene_data = rd.hgetall('genes_data')
+        all_gene_data = rd.hgetall('genes')
         #all_gene_data = []
         #    all_gene_data.append(rd.hgetall(item))
         return all_gene_data
 
     elif request.method == 'POST':
-        response = requests.get(url='https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json').json()
-        
+        post_method()
         return 'Data loaded in\n'
 
     elif request.method == 'DELETE':
-        rd.flushdb()
-        return f'Data deleted, there are {len(rd.keys())} keys in the db\n'
+        delete_method()
+        return f'delete test'
             
     else:
         return 'No method selected. Methods available: GET, POST, DELETE\n'    
